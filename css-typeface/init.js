eb417252dd97e968ad62ae475f893880c57a6adb
jacquelinelau17 <jacqueline.lau.17@gmail.com>
2017-10-23 21:23:10 -0700

    ok

---
+++ css-typeface/init.js
@@ -0,0 +1,130 @@
+var context;
+var $fun;
+var graphic;
+
+
+
+/**
+ *
+ accel events and touch mapped to Synth and Graphic
+ Synth plays notes
+ Graphic does visuals
+ *
+ */
+
+$(document).ready(function(){
+  setup();
+});
+
+var checkFeatureSupport = function(){
+
+  if (!window.DeviceMotionEvent) {
+    alert("DeviveMotionEvent not supported");
+  }
+}
+
+
+var setup = function(){
+  checkFeatureSupport();
+  graphic = new Graphic();
+  $fun = $("#fun");
+
+  //add events
+  $fun.bind("mousedown", touchActivate);
+  $fun.bind("mouseup", touchDeactivate);
+  $fun.bind("touchstart", touchActivate);
+  $fun.bind("touchend", touchDeactivate);
+
+  if (window.DeviceMotionEvent) {
+    window.addEventListener('devicemotion', deviceMotionHandler, false);
+  }
+
+  if (window.DeviceOrientationEvent) {
+    window.addEventListener('deviceorientation', devOrientHandler, false);
+  }
+
+}
+
+
+//touch and gesture mappings to synth and graphic
+//
+var touchActivate = function(e){
+  graphic.touchActivate(e);
+}
+
+var touchDeactivate = function(e){
+  graphic.touchDeactivate(e);
+}
+
+function deviceMotionHandler(eventData) {
+  graphic.accelHandler(eventData);
+}
+
+function devOrientHandler(eventData) {
+  graphic.orientHandler(eventData);
+}
+
+function Graphic(){
+  this.activated = false;;
+  this.background_color="purple";
+}
+
+Graphic.prototype.touchActivate = function(e){
+  this.activated = true;
+  $fun.css("background-color", "lime");
+  $fun.css("background-color", this.background_color);
+}
+
+Graphic.prototype.touchDeactivate = function(e){
+  this.activated = false;
+  $fun.css("background-color","white");
+}
+
+Graphic.prototype.accelHandler = function(accel){
+ var x = accel.accelerationIncludingGravity.x;
+ var y = accel.accelerationIncludingGravity.y;
+ var z = accel.accelerationIncludingGravity.z;
+ var h = x/60.0;
+ var c  = HSVtoRGB(h,1,1);
+ this.background_color = "rgb("+c.r+","+c.g+","+c.b+")" ;
+ if(this.activated){
+    $("#orientation").html("<br />x: "+x+"<br />y: "+y+"<br />z: "+z);
+    $(".item div").css("border-radius", Math.abs(x)*10+"vh");
+    $(".item div").css("border-weight", Math.abs(y)*2+"px");
+    $fun.css("background-color", this.background_color);
+ }
+}
+
+Graphic.prototype.orientHandler = function(orient){
+}
+
+/* accepts parameters
+ * h  Object = {h:x, s:y, v:z}
+ * OR
+ * h, s, v
+*/
+function HSVtoRGB(h, s, v) {
+    var r, g, b, i, f, p, q, t;
+    if (h && s === undefined && v === undefined) {
+        s = h.s, v = h.v, h = h.h;
+    }
+    i = Math.floor(h * 6);
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+    switch (i % 6) {
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
+    return {
+        r: Math.floor(r * 255),
+        g: Math.floor(g * 255),
+        b: Math.floor(b * 255)
+    };
+}
+
---
+++ type2map/assets/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+	$(".cls-18").click(function(){
+        $(".church").hide();
+        $(".church").show();
+    });
+    
+	// $("#Castro").hover(function(){
+ //        $(".church").css("opacity", "1");
+ //        }, function(){
+ //        $(".church").css("opacity", "0");
+ //    });
+
+});
+
